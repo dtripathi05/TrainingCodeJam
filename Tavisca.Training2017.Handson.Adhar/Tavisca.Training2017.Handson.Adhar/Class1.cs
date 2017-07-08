74397578492ab2a92d8ababbc30df073b482cac4
Deependra <dtripathi@tavisca.com>
2017-07-08 09:47:12 +0530

    Created a Project For Adhar

---
+++ Tavisca.Training2017.Handson.Adhar/Tavisca.Training2017.Handson.Adhar/Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tavisca.Training2017.Handson.Adhar
+{
+    public class Input
+    {
+        public void InputOTP(int otp)
+        {
+
+        }
+
+        public void InputAdhar(int Adhar)
+        {
+
+        }
+    }
+    
+    public class Validation
+    {
+        public bool ValidateAdhar()
+        {
+            return true;
+        }
+
+        public bool ValidateOTP()
+        {
+            return true;
+        }
+
+        public bool IsAdharValid()
+        {
+            return true;
+        }
+    }
+
+    public class Session
+    {
+        public void Start()
+        {
+
+        }
+
+        public bool IsActive()
+        {
+            return true;
+        }
+    }
+
+    public class Alert
+    {
+        public void Message()
+        {
+
+        }
+    }
+}
